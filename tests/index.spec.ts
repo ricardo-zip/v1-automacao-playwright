0c210b6213c1c04348c0a4753f5fe6f239357619
Ricardo Dias <diasricardo2001@gmail.com>
2024-12-16 20:24:30 -0300

    changing base url

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,13 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://www.amazon.com.br/');
 
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('has title2', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Amazon.com.br | Tudo pra você, de A a Z./);
 });
