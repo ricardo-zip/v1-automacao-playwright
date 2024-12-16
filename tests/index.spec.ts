9df081fafeca276c32561780641f6cc79272af48
Ricardo Dias <diasricardo2001@gmail.com>
2024-12-16 18:51:13 -0300

    modifying yml files

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
+  await page.goto('https://playwright.dev/');
 
   await expect(page).toHaveTitle(/Playwright/);
 });
