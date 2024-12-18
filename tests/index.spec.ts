f30cab40ef57f04deca4f7276daeb738fc115bbc
Ricardo Dias <diasricardo2001@gmail.com>
2024-12-18 14:42:34 -0300

    finalizing changes v2

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { HomePage } from './pages/home';
 import selectors from './fixtures/selectors.json';
+import { faker } from '@faker-js/faker';
 
 let homePage: HomePage
 
@@ -14,8 +15,23 @@ test.beforeEach(async ({ page }) => {
 })
 
 test.describe('Modal - Teste Grátis', () => {
+  test('Deve realizar uma solicitação de teste grátis realizada com sucesso', async ({ page }) => {
+    await homePage.preencherCamposModal(selectors.input.firstname, faker.name.fullName());
+    await homePage.selecionarOpcao(selectors.input.countryCode, 'BR');
+    await homePage.preencherCamposModal(selectors.input.phone, faker.phone.number());
+    await homePage.preencherCamposModal(selectors.input.email, faker.internet.email());
+    await homePage.selecionarOpcao(selectors.input.positionOrFunction, 'Não sou Advogado');
+    await homePage.selecionarOpcao(selectors.input.companySize, 'De 5 até 10 usuários');
+    await homePage.selecionarOpcao(selectors.input.advocacyChallenge, 'Falta de Integração entre Sistemas');
+    await homePage.selecionarOpcao(selectors.input.officeRevenue, 'Até 50 mil/ano');
+    await homePage.selecionarOpcao(selectors.input.activeProcesses, 'Até 40 processos');
+    await homePage.selecionarOpcao(selectors.input.demonstrationOption, 'Sim');
+    await homePage.cliqueButtonModal('Solicite seu teste')
+    await expect(page.getByText('Obrigado!')).toBeVisible();
+  })
+
   test('Deve validar campos obrigatorios', async ({ page }) => {
-    await homePage.cliqueButtonModal('Solicite seu teste gratuitamente')
+    await homePage.cliqueButtonModal('Solicite seu teste')
     await homePage.validarMenssagemError(selectors.error.firstnameError, 'Preencha esse campo obrigatório');
     await homePage.validarMenssagemError(selectors.error.phoneError, 'Preencha esse campo obrigatório');
     await homePage.validarMenssagemError(selectors.error.emailError, 'Preencha esse campo obrigatório');
@@ -26,20 +42,4 @@ test.describe('Modal - Teste Grátis', () => {
     await homePage.validarMenssagemError(selectors.error.activeProcessesError, 'Preencha esse campo obrigatório');
     await homePage.validarMenssagemError(selectors.error.demonstrationOptionError, 'Preencha esse campo obrigatório');
   });
-
-  test('Deve realizar uma solicitação de teste grátis realizada com sucesso', async ({ page }) => {
-    test.setTimeout(20000);
-    await homePage.preencherCamposModal(selectors.input.firstname, 'Ricardo Dias');
-    await homePage.selecionarOpcao(selectors.input.countryCode, 'BR');
-    await homePage.preencherCamposModal(selectors.input.phone, '11992020304');
-    await homePage.preencherCamposModal(selectors.input.email, 'simple@gmail.com');
-    await homePage.selecionarOpcao(selectors.input.positionOrFunction, 'Não sou Advogado');
-    await homePage.selecionarOpcao(selectors.input.companySize, 'De 5 até 10 usuários');
-    await homePage.selecionarOpcao(selectors.input.advocacyChallenge, 'Falta de Integração entre Sistemas');
-    await homePage.selecionarOpcao(selectors.input.officeRevenue, 'Até 50 mil/ano');
-    await homePage.selecionarOpcao(selectors.input.activeProcesses, 'Até 40 processos');
-    await homePage.selecionarOpcao(selectors.input.demonstrationOption, 'Sim');
-    await homePage.cliqueButtonModal('Solicite seu teste gratuitamente')
-    await expect(page.getByText('Obrigado!')).toBeVisible();
-  })
 })
